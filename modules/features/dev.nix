9f25e10bbc4c3caead3936b8afac7d2fba8f8d36
Eric Chuang <ericchuang94@gmail.com>
2026-05-12 13:45:26 -0700

    dev

--- modules/features/dev.nix
+++ modules/features/dev.nix
@@ -1,15 +1,23 @@
 {inputs, ...}: {
   flake.modules.nixos.dev = {
-    imports = [inputs.self.modules.nixos.opencode];
     home-manager.sharedModules = [inputs.self.modules.homeManager.dev];
   };
 
   flake.modules.homeManager.dev = {pkgs, ...}: {
     home.packages = with pkgs; [
       cursor-cli
+      gemini-cli
       gh
       uv
       clang
     ];
+    programs.opencode = {
+      enable = true;
+      settings = {
+        plugin = ["opencode-gemini-auth@latest"];
+      };
+    };
+    home.sessionVariables.OPENCODE_EXPERIMENTAL_PLAN_MODE = "true";
+    programs.git = {ignores = [".opencode/" ".cursor/"];};
   };
 }
--- modules/features/git.nix
+++ modules/features/git.nix
@@ -18,7 +18,6 @@
         signByDefault = true;
         key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOanOZf0P4dP7gnyYLQ8WcxTm3ln5rFZE+J/1RhTewVR";
       };
-      ignores = [".opencode/"];
     };
   };
 }
