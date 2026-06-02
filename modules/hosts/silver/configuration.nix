89c09fe4ff9bd5f2269238ac85cc322a0692287c
Eric Chuang <ericchuang94@gmail.com>
2026-06-01 18:38:35 -0700

    remove base feature

--- modules/hosts/silver/configuration.nix
+++ modules/hosts/silver/configuration.nix
@@ -2,11 +2,11 @@
   flake.modules.nixos.silver = {
     pkgs,
     config,
+    lib,
     ...
   }: {
     imports = with inputs.self.modules.nixos; [
       agenix
-      base
       cli
       git
       immich
@@ -25,68 +25,94 @@
       zsh
     ];
 
-    nix.settings = {
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      ];
+    options.mainUser = lib.mkOption {
+      type = lib.types.str;
+      default = "ericbreh";
     };
 
-    networking.hostName = "silver";
+    config = {
+      nix.settings = {
+        experimental-features = ["nix-command" "flakes"];
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
+      };
+      nix.extraOptions = "warn-dirty = false";
+      nixpkgs.config.allowUnfree = true;
+      system.stateVersion = "25.05";
 
-    users.users.${config.mainUser} = {
-      isNormalUser = true;
-      description = "Eric Chuang";
-      extraGroups = ["networkmanager" "wheel"];
-      shell = pkgs.zsh;
-      openssh.authorizedKeys.keys = [
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOanOZf0P4dP7gnyYLQ8WcxTm3ln5rFZE+J/1RhTewVR ericchuang94@gmail.com"
-      ];
-    };
+      networking.hostName = "silver";
 
-    home-manager.users.${config.mainUser} = {
-      home.homeDirectory = "/home/${config.mainUser}";
-      home.stateVersion = "25.05";
-      home.packages = with pkgs; [
-        iperf
-      ];
-      tmux.prefix = "b";
-    };
+      users.users.${config.mainUser} = {
+        isNormalUser = true;
+        description = "Eric Chuang";
+        extraGroups = ["networkmanager" "wheel"];
+        shell = pkgs.zsh;
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOanOZf0P4dP7gnyYLQ8WcxTm3ln5rFZE+J/1RhTewVR ericchuang94@gmail.com"
+        ];
+      };
 
-    syncthing.dataDir = "/srv/storage/syncthing";
-    time.timeZone = "America/Los_Angeles";
-    services.tailscale.enable = true;
-    services.logind = {
-      settings = {
-        Login = {
-          AllowSuspend = false;
-          AllowHibernation = false;
-          AllowHybridSleep = false;
-          AllowSuspendThenHibernate = false;
-          HandleLidSwitch = "ignore";
+      home-manager.users.${config.mainUser} = {
+        home.homeDirectory = "/home/${config.mainUser}";
+        home.stateVersion = "25.05";
+        home.packages = with pkgs; [
+          iperf
+        ];
+        tmux.prefix = "b";
+      };
+
+      networking.networkmanager.enable = true;
+      systemd.services.NetworkManager-wait-online.enable = false;
+      zramSwap.enable = true;
+      time.timeZone = "America/Los_Angeles";
+      i18n.defaultLocale = "en_US.UTF-8";
+      services.logind = {
+        settings = {
+          Login = {
+            AllowSuspend = false;
+            AllowHibernation = false;
+            AllowHybridSleep = false;
+            AllowSuspendThenHibernate = false;
+            HandleLidSwitch = "ignore";
+          };
         };
       };
-    };
-    services.tlp = {
-      enable = true;
-      settings = {
-        STOP_CHARGE_THRESH_BAT0 = 55;
-        START_CHARGE_THRESH_BAT0 = 45;
+      services.tlp = {
+        enable = true;
+        settings = {
+          STOP_CHARGE_THRESH_BAT0 = 55;
+          START_CHARGE_THRESH_BAT0 = 45;
+        };
       };
-    };
 
-    fileSystems."/srv/storage" = {
-      device = "/dev/disk/by-uuid/daf86907-e811-4bd1-9baf-fa44f2a83203";
-      fsType = "ext4";
-      options = ["noatime"];
-    };
+      fileSystems."/srv/storage" = {
+        device = "/dev/disk/by-uuid/daf86907-e811-4bd1-9baf-fa44f2a83203";
+        fsType = "ext4";
+        options = ["noatime"];
+      };
 
-    fileSystems."/srv/backup" = {
-      device = "/dev/disk/by-uuid/ae6a911d-ccbf-48fc-a54f-2ba26a036269";
-      fsType = "ext4";
-      options = ["nofail" "noatime"];
+      fileSystems."/srv/backup" = {
+        device = "/dev/disk/by-uuid/ae6a911d-ccbf-48fc-a54f-2ba26a036269";
+        fsType = "ext4";
+        options = ["nofail" "noatime"];
+      };
+
+      boot = {
+        loader = {
+          systemd-boot.enable = true;
+          timeout = 1;
+          efi.canTouchEfiVariables = true;
+        };
+        kernelPackages = pkgs.linuxPackages_latest;
+      };
+
+      syncthing.dataDir = "/srv/storage/syncthing";
+      services.tailscale.enable = true;
+      programs.zsh.enable = true;
     };
   };
 }
--- modules/hosts/titan/configuration.nix
+++ modules/hosts/titan/configuration.nix
@@ -2,10 +2,10 @@
   flake.modules.nixos.titan = {
     pkgs,
     config,
+    lib,
     ...
   }: {
     imports = with inputs.self.modules.nixos; [
-      base
       cli
       comma
       dev
@@ -35,58 +35,84 @@
       zen-browser
     ];
 
-    nix.settings = {
-      extra-substituters = [
-        "https://nix-community.cachix.org"
-        "https://eko-network.cachix.org"
-      ];
-      extra-trusted-public-keys = [
-        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-        "eko-network.cachix.org-1:1xHfovoNlydsTCzXxr5AstUoJUmGR/tRq0PQSCyPab8="
-      ];
+    options.mainUser = lib.mkOption {
+      type = lib.types.str;
+      default = "ericbreh";
     };
 
-    networking.hostName = "titan";
-
-    users.users.${config.mainUser} = {
-      isNormalUser = true;
-      description = "Eric Chuang";
-      extraGroups = ["networkmanager" "wheel" "i2c" "dialout"];
-      shell = pkgs.zsh;
-    };
-
-    home-manager.users.${config.mainUser} = {
-      home.homeDirectory = "/home/${config.mainUser}";
-      home.stateVersion = "25.05";
-    };
-
-    services.tailscale.enable = true;
-    services.automatic-timezoned.enable = true;
-    services.geoclue2.enable = true;
-    services.gvfs.enable = true;
-    services.udisks2.enable = true;
-    services.printing.enable = true;
-    services.fprintd.enable = false;
-    services.power-profiles-daemon.enable = true;
-    services.thermald.enable = true;
-    services.pulseaudio.enable = false;
-    security.rtkit.enable = true;
-    services.pipewire = {
-      enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
-    };
-    hardware = {
-      bluetooth = {
-        enable = true;
-        powerOnBoot = true;
+    config = {
+      nix.settings = {
+        experimental-features = ["nix-command" "flakes"];
+        extra-substituters = [
+          "https://nix-community.cachix.org"
+          "https://eko-network.cachix.org"
+        ];
+        extra-trusted-public-keys = [
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+          "eko-network.cachix.org-1:1xHfovoNlydsTCzXxr5AstUoJUmGR/tRq0PQSCyPab8="
+        ];
       };
-      i2c.enable = true;
-    };
-    services.xserver.xkb = {
-      layout = "us";
-      variant = "";
+      nix.extraOptions = "warn-dirty = false";
+      nixpkgs.config.allowUnfree = true;
+      system.stateVersion = "25.05";
+
+      networking.hostName = "titan";
+
+      users.users.${config.mainUser} = {
+        isNormalUser = true;
+        description = "Eric Chuang";
+        extraGroups = ["networkmanager" "wheel" "i2c" "dialout"];
+        shell = pkgs.zsh;
+      };
+
+      home-manager.users.${config.mainUser} = {
+        home.homeDirectory = "/home/${config.mainUser}";
+        home.stateVersion = "25.05";
+      };
+
+      networking.networkmanager.enable = true;
+      systemd.services.NetworkManager-wait-online.enable = false;
+      zramSwap.enable = true;
+      services.automatic-timezoned.enable = true;
+      services.geoclue2.enable = true;
+      i18n.defaultLocale = "en_US.UTF-8";
+      services.gvfs.enable = true;
+      services.udisks2.enable = true;
+      services.printing.enable = true;
+      services.fprintd.enable = false;
+      services.power-profiles-daemon.enable = true;
+      services.thermald.enable = true;
+      services.pulseaudio.enable = false;
+      security.rtkit.enable = true;
+      services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+      };
+      hardware = {
+        bluetooth = {
+          enable = true;
+          powerOnBoot = true;
+        };
+        i2c.enable = true;
+      };
+      services.xserver.xkb = {
+        layout = "us";
+        variant = "";
+      };
+
+      boot = {
+        loader = {
+          systemd-boot.enable = true;
+          timeout = 1;
+          efi.canTouchEfiVariables = true;
+        };
+        kernelPackages = pkgs.linuxPackages_latest;
+      };
+
+      services.tailscale.enable = true;
+      programs.zsh.enable = true;
     };
   };
 }
