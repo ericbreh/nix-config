6cdc0db626e709c6d941b4a89dacb2373af80766
Eric Chuang <ericchuang94@gmail.com>
2026-08-21 15:35:02 -0400

    abs config

--- modules/features/audiobookshelf.nix
+++ modules/features/audiobookshelf.nix
@@ -4,12 +4,26 @@
       enable = true;
     };
 
-    services.nginx.virtualHosts."${config.duckdns.domain}.duckdns.org".locations."/audiobookshelf" = {
-      proxyPass = "http://127.0.0.1:8000/audiobookshelf";
-      proxyWebsockets = true;
-      extraConfig = ''
-        client_max_body_size 10G;
+    services.nginx = {
+      commonHttpConfig = ''
+        limit_req_zone $binary_remote_addr zone=audiobookshelf_login:10m rate=10r/m;
       '';
+
+      virtualHosts."${config.duckdns.domain}.duckdns.org" = {
+        locations."/audiobookshelf" = {
+          proxyPass = "http://127.0.0.1:8000/audiobookshelf";
+          proxyWebsockets = true;
+          extraConfig = ''
+            client_max_body_size 10G;
+          '';
+        };
+        locations."/audiobookshelf/login" = {
+          proxyPass = "http://127.0.0.1:8000/audiobookshelf/login";
+          extraConfig = ''
+            limit_req zone=audiobookshelf_login burst=5 nodelay;
+          '';
+        };
+      };
     };
   };
 }
